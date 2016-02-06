b5dcf1c22f7696676795b7c18d5f16f8e6668aff
itoshinsuke <itoshinsuke@geroMcBkPro.local>
2016-02-06 23:54:29 +0900

    initial commit 一通りの機能を揃えられた

---
+++ dataHandle/dataHandle.js
@@ -0,0 +1,194 @@
+// dataHandle
+// --csvParse
+// --ArrayArray
+// --ObjectArray
+//1602061617
+
+// nameSpace
+var dataHandle;
+if (!dataHandle){
+  dataHandle = {};
+}
+// =======================================================
+// csvParse Method
+// =======================================================
+// parseInto ObjectArray or ArrayArray
+// strSrc				source
+// isUseHeader	use a firstline as a header
+// ...returns		isUseHeader:true   ObjectArray
+// ...returns		isUseHeader:false  ArrayArray
+dataHandle.csvParse = function(strSrc, chrDelimiter, isUseHeader){
+  var tempData = [];
+  var lines = strSrc.split('\n');
+  if (isUseHeader){
+    // parse As an ObjectArray ---------
+    if (lines.length < 2) return;
+    var heads = lines[0].split(chrDelimiter);
+    for (var i = 1; i < lines.length; i ++){
+      // emptyline ; skip
+      if (lines[i] == '') break;
+
+      // split by delimiter
+      var vals = lines[i].split(chrDelimiter);
+      if (vals.length  == 0 ) break;
+
+      // add object
+      tempData[i-1] = {};
+      for (var j = 0; j < vals.length; j++){
+        tempData[i-1][heads[j]] = vals[j];
+      }
+    }
+    return new this.ObjectArray(tempData);
+  }else{
+    // parse as an ArrayArray -----------
+    if (lines.length < 1) return;
+    for (var i = 0; i < lines.length; i ++){
+      // emptyline ; skip
+      if (lines[i] == '') break;
+
+      // split by delimiter
+      var vals = lines[i].split(chrDelimiter);
+      tempData[i]=[];
+      for (var j = 0; j < vals.length; j++){
+        tempData[i][j] = vals[j];
+      }
+    }
+    return new this.ArrayArray(tempData);
+  }
+}
+
+// =======================================================
+// ObjectArray Class
+// =======================================================
+  dataHandle.ObjectArray = function(data){
+    this.data = data;
+  }
+  // 該当する行に絞るフィルタをかけて新しいObjectArrayを生成するメソッド
+  dataHandle.ObjectArray.prototype.filterRow = function(findLabel, findValue){
+    // 編集用のデータを用意
+    var tempData = this.data;
+    
+    // 検索指定があれば対象行をしぼる
+    if (findLabel && findValue){
+      tempData = tempData.filter(function(element, index, array){
+      	// 該当フィールド見当たらなければ終了
+        if (!element[findLabel]) return;
+        
+        if ( Array.isArray(findValue) ){
+          //  配列　配列のいずれかに等しい
+          return findValue.some(function(value){
+            return element[findLabel] === value;
+          });
+          
+        }else if (findValue.substr(0,2) == '*='){
+        　// "*=VALUE" VALUEを含む
+          return element[findLabel].indexOf(findValue.slice(2)) != -1 ;
+          
+        }else{
+          // "VALUE" VALUEに等しい
+          return element[findLabel] === findValue;
+        }
+      })
+    }
+    // // 編集用のデータを用意
+    // var tempData = this.data;
+    //
+    // // 検索指定があれば対象行をしぼる
+    // if (findLabel && findValue){
+    //   tempData = tempData.filter(function(element, index, array){
+    //     return element[findLabel] === findValue;
+    //   })
+    // }
+    return new this.constructor(tempData);
+  };
+
+  dataHandle.ObjectArray.prototype.sortRow = function( sortLabel, isAscend){
+    // データの対象
+    var tempData = this.data;
+
+    // 昇降フラグを整理する
+    if (isAscend === undefined ) isAscend = true;
+    var flag =  ( isAscend ) ? 1 : -1 ;
+
+    // ソートする
+    tempData.sort(function(v1, v2){
+      // 数値
+      return (v1[sortLabel] - v2[sortLabel]) * flag;
+      // // 文字列
+      // if (v1[sortLabel] < v2[sortLabel]){
+      //   return -1 * flag;
+      // }else {
+      //   return  1 * flag;
+      // }
+    });
+
+    return this;
+  }
+
+  //	任意のカラムを配列として返すメソッド
+  // dataHandle.ObjectArray.prototype.getList = function(getLabel, isUnique, findLabel, findValue){
+  dataHandle.ObjectArray.prototype.getList = function(getLabel, isUnique, findLabel, findValue, sortLabel, isAscend){
+    // 編集用のデータを取得
+    var tempObjArr = this;
+
+    // 検索指定があれば対象行をしぼる
+    if (findLabel && findValue){
+      tempObjArr = tempObjArr.filterRow(findLabel, findValue);
+    }
+
+    // ソート指定があればソートする
+    if ( sortLabel ){
+      tempObjArr.sortRow(sortLabel, isAscend);
+    }
+
+    // 取得したいラベルのみのリストを作成する
+    var tempList = [];
+    tempList = tempObjArr.data.map(function(element, index, array){
+      return element[getLabel];
+    });
+
+    // 一意データフラグがONなら
+    if (isUnique){
+      tempList = tempList.filter(function(element, index, array){
+        return array.indexOf(element) === index;
+      });
+    }
+    return tempList;
+  };
+
+// =======================================================
+// ArrayArray Class
+// =======================================================
+  dataHandle.ArrayArray = function(data){
+    this.data = data;
+  };
+
+  // 指定フィールドが特定の値をとる行のみ取得する
+  dataHandle.ArrayArray.prototype.filterRow = function(filterId, filterValue){
+      var tempArr = this.data;
+      if ( !element[filterId]) return;
+      tempArr = tempArr.filter(function(element){
+          return element[filterId]==filterValue;
+      });
+      return new this.constructor(tempArr);
+  }
+  //該当する行の指定フィールドを配列として取得する
+  dataHandle.ArrayArray.prototype.getList = function(findId, isUnique, filterId, filterValue){
+      var temp = this;
+      // フィルタがあればフィルタをかける
+      if (filterId && filterValue){
+        temp = this.filterRow(filterId, filterValue);
+      }
+
+      // 指定のＩＤの行をとりだす
+      tempArr = temp.data.map(function(element){
+          return element[findId];
+      });
+      // ユニークフラグがあれば一意のデータのみにする
+      if(isUnique){
+          tempArr = tempArr.filter(function(element, index, array){
+              return array.indexOf(element) == index;
+          });
+      }
+      return tempArr;
+  }
---
+++ geoHandle/geoHandle.js
@@ -0,0 +1,228 @@
+//  TODO
+
+//////////////////////////////////////////////////////////////////////
+// Class Geohandle
+//          require ttp://maps.google.com/maps/api/js?language=ja
+//          require optinal window.localStorage
+//          require optinal window.JSON
+//////////////////////////////////////////////////////////////////////
+'use strict'
+var GeoHandle ={
+  geocoder:{},
+  listAddress:[],   //連想配列オブジェクト　キーは登録時の住所文字列  プロパティも住所文字列
+  cacheResult:{},   //連想配列オブジェクト　キーは登録時の住所文字列
+                    //プロパティにはlatlng:{lat:0,lng:0}, address:""
+  nowRequest: "",   //検索中の住所
+  onLocate:false,	  //アイテムが取得される度に呼ばれるcallback(geocoderResult)
+  dummy:'',
+  urgentCount:0,  //急ぎのアイテムの数
+  TIME_QUERYOVER: 300,
+  TIME_NOTURGENT: 1200,
+};
+
+//	初期化する
+GeoHandle.init = function (){
+  GeoHandle.geocoder = new google.maps.Geocoder();
+  GeoHandle.resetList();
+}
+
+//  リストをリセットする
+GeoHandle.resetList = function(){
+  GeoHandle.listAddress = [];
+  GeoHandle.cacheResult = {};
+}
+
+//	アドレスを追加する
+//  param   文字列、または文字列配列
+//  isUrgent  急ぎかどうか    trueならunshiftで順番待ちに入れる
+GeoHandle.addAddress = function(param, isUrgent){
+
+  if(Array.isArray(param)){
+    //配列の場合
+    if(!isUrgent){
+      GeoHandle.listAddress = GeoHandle.listAddress.concat(param);
+    }else {
+      GeoHandle.listAddress = param.concat(GeoHandle.listAddress);
+      GeoHandle.urgentCount += param.length;
+    }
+
+  }else if (typeof param==='string'){
+    //文字列の場合
+    if(!isUrgent){
+      GeoHandle.listAddress.push(addParam);
+    }else {
+      GeoHandle.listAddress.unshift(addParam);
+      GeoHandle.urgentCount++;
+    }
+  // その他
+  }else{
+    return;
+  }
+};
+
+//
+GeoHandle.doNextSearch = function (){
+  // 次検索アイテムをセットする
+  if (!GeoHandle.prepareNextRequest()){
+    //	falseなら終端。終了。
+     return;
+  }else {
+    //	次を検索する
+    GeoHandle.doSearch();
+  }
+};
+
+//next
+GeoHandle.prepareNextRequest = function(){
+  // リストが０ならばfalseを返す
+  if (GeoHandle.listAddress.length <= 0) {
+    return false;
+  }
+  // キーを取得
+  if (GeoHandle.urgentCount > 0) GeoHandle.urgentCount--;
+
+  //	すでに検査済ならスキップ
+  var key = GeoHandle.listAddress.shift();
+  if (GeoHandle.cacheResult[key]){
+    return GeoHandle.prepareNextRequest();
+  }
+  //	リクエストのセット
+  GeoHandle.nowRequest = key;
+  return true;
+};
+
+
+// サーチを開始する
+GeoHandle.doSearch = function (){
+  // 現在調べ中の住所を取得する
+  var request = GeoHandle.nowRequest;
+
+  // locaclStorageから該当を探す
+  var result = GeoHandle.getLocalStorage(request);
+  if (result ){
+    // 結果の取得に成功した時アクション
+    GeoHandle.onGetResult(request, result);
+  }else {
+    // localStorageに該当がない場合　googlemapのgeocoderを実行する
+    GeoHandle.geocoder.geocode({address: request}, GeoHandle.searchAddress);
+  }
+}
+// ジオコード結果の取得に成功した時のアクションを実行する
+GeoHandle.onGetResult = function(request, result){
+
+  // localStorageに格納
+  GeoHandle.setLocalStorage(request, result);
+
+  // Resultをキャッシュに格納
+  GeoHandle.cacheResult[request] = result;
+
+  // ユーザー指定のコールバックに飛ぶ
+  if (GeoHandle.onLocate)	GeoHandle.onLocate(request, result);
+
+  // 次の検索を開始する
+  GeoHandle.doNextSearch();
+  // if(GeoHandle.urgentCount){
+  //   GeoHandle.doNextSearch();// 急ぎならすぐに
+  // }else{
+  //   急ぎでなければちょっと間をおいて行う
+  //   setTimeout(GeoHandle.doNextSearch, GeoHandle.TIME_NOTURGENT);　
+  // }
+}
+
+//geocode callback
+GeoHandle.searchAddress = function (results, status){
+  // 処理中の住所取得
+  var request = GeoHandle.nowRequest;
+
+  // GeocoderステータスがOKならResult{address, latlng}を取得する
+  var saveResult = GeoHandle.getResultIfStatusOK(results, status);
+  if (!saveResult ){
+    //クエリーオ-バー時は再度やり直す
+    setTimeout(GeoHandle.doSearch,GeoHandle.TIME_QUERYOVER);
+    return;
+  }
+  // ジオコード結果の取得に成功した時のアクションを実行する
+  GeoHandle.onGetResult(request, saveResult);
+
+}
+
+//
+GeoHandle.getLocalStorage = function(key){
+  // ブラウザがサポートしていなければ終了
+  if (!localStorage || !JSON ){return;}
+  // ストレージから情報を取得
+  var Result = JSON.parse(localStorage.getItem(key));
+  if (Result ){
+    // JSONで戻したデータからLatLngオブジェクトを再生成する
+    var lat = Result.latlng.lat;
+    var lng = Result.latlng.lng;
+    Result.latlng = new google.maps.LatLng(lat, lng);
+    // result を返す
+    return Result;
+  }
+}
+
+GeoHandle.setLocalStorage = function(key, saveResult){
+  // ブラウザがサポートしていなければ終了
+  if (!localStorage || !JSON ){return;}
+  // 情報をjson形式にして格納
+  localStorage.setItem(key, JSON.stringify(saveResult));
+}
+
+
+
+// TODO この関数いる？searchAddress内に入れた方がいい？
+GeoHandle.getResultIfStatusOK =function(results, status){
+  switch(status){
+    case google.maps.GeocoderStatus.OK:
+    //  成功の場合
+      var saveResult = {};
+      saveResult['latlng'] = results[0].geometry.location;
+      saveResult['address'] = GeoHandle.getGcrAddressText(results[0]);
+      return saveResult;
+    case google.maps.GeocoderStatus.OVER_QUERY_LIMIT:
+    //  クエリーを出しすぎて怒られた場合
+      return false;　        // falseでリターンする
+    default:
+    // その他エラーの場合 // TODO エラー処理を書く
+      return GeoHandle.getEmptyResult();// 失敗用のデータをcacheに格納
+  }
+}
+
+//  失敗したときの空のGoogleGeocoderResultをかえす
+GeoHandle.getEmptyResult = function(){
+  //return false;
+  return ;//{latlng:"failed!", address:"failed!"};
+}
+
+//GeocodeResultから適度長さの（市町村以下の）住所名を取得する
+GeoHandle.getGcrAddressText = function(gcResult){
+  var stradr="";
+  for(var i=0; i < gcResult.address_components.length; i++){
+    var cmpnt = gcResult.address_components[i]
+    var lname = cmpnt.long_name;
+
+    // 挿入のあれこれ
+    if (cmpnt.types[0] =='premise'){
+                                  //マンション名はなにもしない
+    }else if (stradr.length == 0){
+      stradr = lname;             //一つ目はとりあえず入れる
+    }else if (lname.match(/[^0-9０-９]/)==null){
+        stradr = lname + '－' + stradr;   //数字だけなら間にハイフンをはさんで追加する
+    }else {
+        stradr = lname + stradr;    //それ以外は普通に追加する
+    }
+
+    // 番地以上のものをいれたらそこで終わりにする
+    if (stradr.length > 0 &&
+      cmpnt.types[0].match(/sublocality_level_[2-9]/)==null) break;
+  }
+  if (stradr　== '１'){
+    console.log("a");
+  }
+  return stradr;
+}
+
+//
+// END	class Geohandle
+//////////////////////////////////////////////////////////////////////
